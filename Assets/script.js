8f5d414f106d209d6a5358f9d893b4b8b70f2282
Jesse <jesse.lare@gmail.com>
2023-08-30 23:40:38 -0400

    intital code commit

---
+++ Assets/script.js
@@ -0,0 +1,23 @@
+// Countdown Logic
+const countDownDate = new Date("Sep 30, 2023 00:00:00").getTime();
+let x = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "It's Party Time!";
+  }
+}, 1000);
+
+// Surprise button logic
+document.getElementById('surpriseBtn').addEventListener('click', function() {
+  document.getElementById('surprise').style.display = 'block';
+});
